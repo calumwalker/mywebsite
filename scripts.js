fc85e11d758f7f35d2b7477d5f93abbc10be814a
calumwalker <34166693+calumwalker@users.noreply.github.com>
2017-12-08 13:19:18 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,23 +1,11 @@
-//$("img.hamburger").click(function(){
 
-//$("div.overlay").fadeIn();
-//$("div.overlay").slideDown();
-
-//});
-//$("img.close").click(function(){
-
-////$("div.overlay").fadeOut();
-//$("div.overlay").slideUp();
-
-
-
-
-//});
 
 
 
 $( document ).ready(function() {
-
+//javascript so that the burger menu works
+// so that when the hamburger icon is clicked, the overlay displays and the close icon displays while the hamburger (open ) icon
+// dissapears
 $( ".close" ).hide();
 $( ".overlay" ).hide();
 $( ".hamburger" ).click(function() {
@@ -26,7 +14,7 @@ $( ".hamburger" ).hide();
 $( ".close" ).show();
 });
 });
-
+//when the close icon is pressed then the overlay and the close icon dissapear and then the hamburger (open) icon is displayed
 $( ".close" ).click(function() {
 $( ".overlay" ).slideToggle( "slow", function() {
 $( ".close" ).hide();
